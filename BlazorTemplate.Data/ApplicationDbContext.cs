47048257ef8c470c1e187aa258ed5f7509c4abe7
Nathan Tripp <ntripp@workfirstcasualty.com>
2025-04-02 13:01:06 -0600

    Enhance documentation

--- BlazorTemplate.Data/ApplicationDbContext.cs
+++ BlazorTemplate.Data/ApplicationDbContext.cs
@@ -10,12 +10,18 @@ namespace BlazorTemplate.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options) {
     public const string ConnectionString = "Data Source=app.db";
 
+    /// <summary>
+    /// Represents a collection of Worker entities in the database. Allows for querying and managing worker data.
+    /// </summary>
     public virtual DbSet<Worker> Workers { get; set; }
+
+    /// <summary>
+    /// Represents a collection of Company entities in the database. Provides functionality to query and save Company data.
+    /// </summary>
     public virtual DbSet<Company> Companies { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
-
 
         modelBuilder.Entity<Company>(c =>
             c.HasData(new Company() {
